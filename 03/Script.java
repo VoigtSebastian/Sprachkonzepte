ae89298b39ef3dc21cfa6836eedfdb86f8713a67
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2021-12-20 14:46:26 +0100

    Add duplicate function error

--- 03/Script.java
+++ 03/Script.java
@@ -1,19 +1,28 @@
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class Script extends Expr {
-    final LinkedList<FunctionDefinition> functions = new LinkedList<>();
+    final Map<String, FunctionDefinition> functions = new HashMap<>();
+
+    public final class DuplicateFunctionError extends RuntimeException {
+        public DuplicateFunctionError(String identifier) {
+            super("Function with name the name \"" + identifier + "\" does already exist.");
+        }
+    }
 
     public Script() {
     }
 
     public void addFunctionDefinition(FunctionDefinition function) {
-        functions.add(function);
+        if (this.functions.containsKey(function.getIdentifier()))
+            throw new DuplicateFunctionError(function.getIdentifier());
+        functions.put(function.getIdentifier(), function);
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        for (Expr expr : this.functions) {
+        for (Expr expr : this.functions.values()) {
             builder.append(expr.toString());
             builder.append("\n");
         }
