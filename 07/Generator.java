8bc27948e6b1c0f6529cea5f41374f619d302b35
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2022-01-26 20:27:00 +0100

    Add working main template

--- 07/Generator.java
+++ 07/Generator.java
@@ -1,7 +1,16 @@
-import org.stringtemplate.v4.*;
+import java.util.LinkedList;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroupFile;
 
 public final class Generator {
-    public static void main(String[] args) {
-        System.out.println("hi");
+    public static void main(String[] args) throws Exception {
+        var list = new LinkedList<Class<?>>();
+        list.add(Class.forName("java.lang.String"));
+
+        ST template = new STGroupFile("aufgabe7.stg").getInstanceOf("classInfo");
+        template.add("list", list);
+        String result = template.render();
+        System.out.println(result);
     }
 }
