c2b4dd39ee86dc858fce9cb53e2433046a0a8a9d
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2022-01-26 15:08:09 +0100

    Update exercise 6c

--- 06/6c_zug_inprogress.pl
+++ 06/6c_zug_inprogress.pl
@@ -1,24 +1,17 @@
-/*Vorgegebene Verbindungen*/
-zug(konstanz, 08.40, offenburg, 10.59).
 zug(konstanz, 08.40, karlsruhe, 11.49).
-zug(konstanz, 08.53, singen, 09.26).
-zug(singen, 09.37, stuttgart, 11.32).
-zug(offenburg, 11.29, mannheim, 12.24).
 zug(karlsruhe, 12.08, mainz, 13.47).
-zug(stuttgart, 11.51, mannheim, 12.28).
-zug(mannheim, 12.39, mainz, 13.18).
 
-/* Test Verbindung aufgrund doppelt */
-zug(singen, 12.30, mainz, 14.47).
+% Check if a connection from START to DESTINATION exists, that starts after ERLIEST
+% Append to LIST if the predicate above is true
+verbindung(START,EARLIEST,DESTINATION,LIST):-
+    zug(START, TIME, DESTINATION, ARRIVAL),
+    TIME>=EARLIEST,
+    CONNECTION=[START, TIME, DESTINATION, ARRIVAL],
+    append(CONNECTION, [], LIST).
 
+% Check if there is a connection from START to somewhere, which satisfies the predicate above
+% Check if there is a connection from somewhere to DESTINATION
+verbindung(START,EARLIEST,DESTINATION,LIST):-
+    verbindung(START, EARLIEST, X, LIST),
+    verbindung(X, EARLIEST, DESTINATION, LIST).
 
-
-verbindung(ABORT,ABZEIT,ANORT,LIST):-
-    zug(ABORT,ZABZEIT, USTORT, USTZEIT),
-    ABZEIT < ZABZEIT,
-    zug(USTORT,ZUSTZEIT, ANORT, ANZEIT),
-    ZUSTZEIT>=USTZEIT,
-    VB = [ABORT, ZABZEIT, ANORT, ANZEIT],
-    %not(member(VB, LIST)),
-    %append(VB, LIST, LIST).
-    LIST=[VB|LIST].
