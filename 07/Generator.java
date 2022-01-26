1458b7e4353ad498cc9d7f9a38d91b209b22150b
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2022-01-26 21:05:47 +0100

    Add working example

--- 07/Generator.java
+++ 07/Generator.java
@@ -5,12 +5,21 @@ import org.stringtemplate.v4.STGroupFile;
 
 public final class Generator {
     public static void main(String[] args) throws Exception {
-        var list = new LinkedList<Class<?>>();
-        list.add(Class.forName("java.lang.String"));
+        var list = new LinkedList<ClassInfo>();
+        list.add(new ClassInfo(Class.forName("java.lang.String")));
 
-        ST template = new STGroupFile("aufgabe7.stg").getInstanceOf("classInfo");
+        ST template = new STGroupFile("aufgabe7.stg").getInstanceOf("aufgabe7");
         template.add("list", list);
+
         String result = template.render();
         System.out.println(result);
+    }
+}
+
+final class ClassInfo {
+    public final String name;
+
+    public ClassInfo(Class<?> c) {
+        this.name = c.getName();
     }
 }
